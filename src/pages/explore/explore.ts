3b70db3c679c87bbf19934cf89cc33b0a05c82e0
fjonathan <jfalcon@marnagarage.com>
2018-06-18 12:08:21 -0400

    cambios en el explorador

--- src/pages/explore/explore.ts
+++ src/pages/explore/explore.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { publicationProvider } from '../../providers/publication/publication';
+import { UserProvider } from '../../providers/user/user';
+import { Storage } from '@ionic/storage';
 /**
  * Generated class for the ExplorePage page.
  *
@@ -12,14 +14,67 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-explore',
   templateUrl: 'explore.html',
+  
 })
 export class ExplorePage {
+  searchQuery: string = '';
+  items: string[];
+   publications: any;
+    user:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public publicationProvider: publicationProvider,
+              public userProvider: UserProvider,
+              public storage: Storage,) {
+     this.publications=[];
+     this.user=[];
+     this.initializeItems();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ExplorePage');
+
+    this.storage.get('user').then((val) => {  
+      if(val){
+        var par = JSON.parse(val);
+        this.user = par;
+        console.log(this.user);
+        console.log(this.user.idString);
+        this.publicationProvider.getpublications(this.user.idString)
+        .then((result) => {
+          console.log(result);
+          this.publications= result['items'];
+          //this.loader.dismiss();
+        }, (err) => {
+          console.log(err);
+          //this.loader.dismiss();
+        });
+      }
+    });  
+  }
+
+
+initializeItems() {
+    this.items = [
+      'Computacion',
+      'Celulares',
+      'Ropa',
+    ];
+  }
+   getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
